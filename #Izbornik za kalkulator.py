296af6c5d8c34a98f7192f22fbfb78d3896a29b8
Marko469 <markolegovic2009@gmail.com>
2025-09-29 14:12:14 +0200

    Add files via upload

---
+++ #Izbornik za kalkulator.py
@@ -0,0 +1,30 @@
+#Izbornik za kalkulator
+print("             ")
+print("Izbornik za kalkulator")
+print("       ")
+print("1.izračunaj napon struje")
+print("2. Izračunaj otpor struje")
+print("3. Izračunaj jakosti struje")
+print("    ")
+opcija = int(input("Izaberite operaciju (1/2/3):"))
+#struktura grananja
+if opcija == 1:
+    print("Izračun napona struje")
+    jakost = int(input("Upiši jakost struje:  "))
+    otpor = int(input("Upiši otpor:   "))
+    napon = jakost*otpor
+    print(f"Napon struje je:  {napon} V")
+elif opcija == 2:
+    print("Izračun otpora struje")
+    napon = int(input("Upiši napon:  "))
+    jakost = int(input("Upiši jakost struje: "))
+    otpor = napon/jakost
+    print(f"Otpor je {otpor} ohm")
+elif opcija== 3:
+    print("Izračunaj jakosti struje")
+    napon = int(input("Upiši napon:  "))
+    otpor = int(input("Upiši otpor:  "))
+    jakost = napon/otpor
+    print(f"Jakost struje je: {jakost} A")
+else:
+    print("Pogrešan unos")
